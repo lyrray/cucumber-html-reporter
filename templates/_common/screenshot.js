ee67d412c8eb4f14f740329201ecf442d816e9c7
lyrray <lyrray@aol.com>
2018-01-04 00:06:59 +0100

    display multiple screenshot under steps

--- templates/_common/screenshot.js
+++ templates/_common/screenshot.js
@@ -1,9 +1,9 @@
-$('a.toggle').on('click', function() {
-    if ($(this).text() === 'Screenshot -') {
-        $(this).text('Screenshot +');
-        $(this).next('a.screenshot').find('img').hide();
-    } else {
-        $(this).text('Screenshot -');
-        $(this).next('a.screenshot').find('img').show();
-    }
+$('a.toggle').on('click', function () {
+	if ($(this).text() === 'Screenshot -') {
+		$(this).text('Screenshot +');
+		$(this).nextAll('a.screenshot').find('img').hide();
+	} else {
+		$(this).text('Screenshot -');
+		$(this).nextAll('a.screenshot').find('img').show();
+	}
 });
